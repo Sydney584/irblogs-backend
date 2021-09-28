29bb857b3d4d57171b62b0f942da8ff98084ed11
sydney <sydneychase5841@gmail.com>
2021-09-28 14:31:08 -0400

    implemented show user by id action method in user controller, tested in postman/browser, GET request for show working properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,15 @@ class UsersController < ApplicationController
     users = User.all
     render json: users
   end
+
+# GET /users/:id
+def show
+  user = User.find_by(id: params[:id])
+  if user
+    render json: user
+  else
+    render json: { error: "User not found" }, status: :not_found
+  end
+end
+
 end
