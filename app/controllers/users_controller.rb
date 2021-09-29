276f84614b34b8baab51fc3179f385f6b8cb1c34
sydney <sydneychase5841@gmail.com>
2021-09-29 01:45:31 -0400

    updated GET/users method & GET/users/:id, added render message & user params permit to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,32 @@
 class UsersController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     # GET /users
   def index
     users = User.all
-    render json: users
+    render json: users, only: [:first_name, :user_name]
   end
   
 
 # GET /users/:id
 def show
-  user = User.find_by(id: params[:id])
-  if user
-    render json: user
-  else
-    render json: { error: "User not found" }, status: :not_found
-  end
+  user = User.find(params[:id])
+  render json: user, include: :blogposts
 end
 
+
+private
+
+  
+
+  def render_not_found_response
+    render json: { error: "User not found" }, status: :not_found
+  end
+      
+  def user_params
+    params.permit(:first_name, :last_name, :user_name, :email, :userid, :blogpost)
+  end
+
 end
+
