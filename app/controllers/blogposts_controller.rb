c4f1e00818e416721e4041267d8b574ea20e2523
sydney <sydneychase5841@gmail.com>
2021-09-29 01:24:54 -0400

    added GET/blogpost/:id method to blogpost controller

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -5,4 +5,15 @@ class BlogpostsController < ApplicationController
     blogposts = Blogpost.all
     render json: blogposts
   end
+
+# GET /blogposts/:id
+def show
+    blogpost = Blogpost.find_by(id: params[:id])
+    if blogpost
+      render json: blogpost
+    else
+      render json: { error: "Blogpost not found" }, status: :not_found
+    end
+  end
+
 end
