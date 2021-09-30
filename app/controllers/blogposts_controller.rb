d34f9389c89f3bd194c6656078f538f8f21595c9
sydney <sydneychase5841@gmail.com>
2021-09-30 14:02:48 -0400

    updated users/blogposts routes and bloposts/users index and show actions in controllers

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -5,7 +5,7 @@ class BlogpostsController < ApplicationController
 
      # GET /blogposts
   def index
-    blogposts = Blogpost.all
+    blogposts = Blogpost.all.order(content: :desc)
     render json: blogposts, include: :comments
   end
 
