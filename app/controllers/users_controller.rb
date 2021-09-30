74c3a1d2c211b27453c2f3a1d832937f34b6e243
sydney <sydneychase5841@gmail.com>
2021-09-30 13:01:51 -0400

    updated user controller w/recordinvalid error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     # GET /users
   def index
@@ -18,10 +19,12 @@ end
 
 private
 
-  
-
   def render_not_found_response
     render json: { error: "User not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
       
   def user_params
